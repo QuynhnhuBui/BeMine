42770a3075bfdfe2a9f116371e825dd41c1d5778
Nhu Bui <buinhu2103@gmail.com>
2019-12-03 12:57:02 +0200

    Dialog

--- app/src/main/java/com/example/bemine/mainscreen.java
+++ app/src/main/java/com/example/bemine/mainscreen.java
@@ -1,5 +1,6 @@
 package com.example.bemine;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -11,46 +12,51 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
+import java.util.Set;
 
 public class mainscreen extends AppCompatActivity {
+
     SharedPreferences sharedPreferences;
-    ConstraintLayout dateLayout;
-    ConstraintLayout mainLayout;
-    TextView textView;
-    DatePicker simpleDatePicker;
-    Button submitButton;
-    TextView dayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
 
-        textView = findViewById(R.id.textView);
-        dayTextView = findViewById(R.id.dayTextView);
-        simpleDatePicker = findViewById(R.id.simpleDatePicker);
-        submitButton = findViewById(R.id.submitButton);
-        dateLayout = findViewById(R.id.dateLayout);
-        mainLayout = findViewById(R.id.mainLayout);
-
-        submitButton.setVisibility(View.VISIBLE);
-        simpleDatePicker.setVisibility(View.VISIBLE);
-        mainLayout.setVisibility(View.INVISIBLE);
-
-
-
+        sharedPreferences = this.getSharedPreferences("com.example.bemine",Context.MODE_PRIVATE);
+        String startDay = sharedPreferences.getString("startDay","Error");
+        Log.d("Nhu", startDay);
+        if(startDay.equals("Error")) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(mainscreen.this);
+            View mView = getLayoutInflater().inflate(R.layout.diaalog_day, null);
+            EditText mDay = mView.findViewById(R.id.dayEditText);
+            Button mSubmit = mView.findViewById(R.id.button);
+            mSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String startDay = mDay.getText().toString();
+                    sharedPreferences.edit().putString("starDay",startDay).apply();
+                }
+            });
+            mBuilder.setView(mView);
+            AlertDialog alertDialog = mBuilder.create();
+            alertDialog.show();
+        }
     }
 
-    public void mainLayout (View view){
-        submitButton.setVisibility(View.INVISIBLE);
-        mainLayout.setVisibility(View.VISIBLE);
-    }
-    public void getDay(View view){
+
+    public void getDay(String startDay){
 
     }
 }
